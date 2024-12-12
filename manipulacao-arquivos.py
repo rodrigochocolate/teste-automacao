2bd0a6f37336f4712ddb3547dd74b2edc7180b59
Rodrigo <rodrigodev2021@gmail.com>
2024-12-12 13:06:40 -0300

    interface gráfica adicionada

--- manipulacao-arquivos.py
+++ manipulacao-arquivos.py
@@ -1,10 +1,32 @@
-# LE O NOME DE UM ARQUIVO
-# LE O NOME DE OUTRO ARQUIVO EXISTENTE OU NÃO
-# COPIA O TEXTO DO PRIMEIRO ARQUIVO NO SEGUNDO
+import tkinter as tk
 
-print('COLOQUE A EXTENSÃO DO ARQUIVO')
-copiado = str(input('Primeiro arquivo: '))
-colado = str(input('Segundo arquivo: '))
 
-with open(copiado) as texto, open(colado, 'w') as novo:
-    novo.write(texto.read())
+def on_button_click():
+    copiado = caixa1.get()
+    colado = caixa2.get()
+    
+    with open(copiado) as texto, open(colado, 'w') as novo:
+        novo.write(texto.read())
+        
+    caixa1.delete(0, tk.END)
+    caixa2.delete(0, tk.END)
+    
+    
+root = tk.Tk()
+root.title('CLONADOR DE TEXTO')
+root.geometry('600x400')
+
+label = tk.Label(root, text='COLOQUE A EXTENSÃO DO ARQUIVO')
+label.pack()
+
+caixa1 = tk.Entry(root, width=50)
+caixa1.pack(pady=20)
+    
+caixa2 = tk.Entry(root, width=50)
+caixa2.pack(pady=20)
+    
+botao = tk.Button(root, text='ENVIAR', command=on_button_click)
+botao.pack()
+
+
+root.mainloop()
